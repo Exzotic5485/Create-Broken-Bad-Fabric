04b5f56b93a1faf679c86ee6444bebce077ca884
Exzotic5485 <61587328+Exzotic5485@users.noreply.github.com>
2023-10-20 12:24:27 +0100

    Update CreateBrokenBad.java

--- src/main/java/com/jetpacker06/CreateBrokenBad/CreateBrokenBad.java
+++ src/main/java/com/jetpacker06/CreateBrokenBad/CreateBrokenBad.java
@@ -6,13 +6,9 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CreateBrokenBad implements ModInitializer, ClientModInitializer {
     public static final String MOD_ID = "createbb";
-
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
